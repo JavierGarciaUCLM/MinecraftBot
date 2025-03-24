70edc076a9b514685443e8906ea0a0be67151f0c
Javier Garcia <161944838+JavierGarciaUCLM@users.noreply.github.com>
2025-03-24 03:29:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,6 @@ require('dotenv').config(); // Carga variables de .env
 // 2. Configuración del bot de Minecraft
 let mcBot; // Variable global para el bot de Minecraft
 
-const mensajesAleatorios = [
-  'I love the Spanish!',
-  'Best clan? The Inquisition.',
-  'Inquisition is always back, no matter what!',
-  'Inquisition won',
-  'We miss you npintea!',
-  'Nikoxlas? Who is him?'
-];
-
 function createMinecraftBot() {
   mcBot = mineflayer.createBot({
     host: process.env.MC_HOST,
@@ -29,17 +20,6 @@ function createMinecraftBot() {
 
   mcBot.on('spawn', () => {
     console.log('Bot de Minecraft conectado.');
-    setInterval(() => {
-      // 3. Escoge un mensaje aleatorio
-      const mensaje = mensajesAleatorios[Math.floor(Math.random() * mensajesAleatorios.length)];
-  
-      // 4. Envía el mensaje al chat de Minecraft
-      mcBot.chat(mensaje);
-  
-  const channel = discordClient.channels.cache.get(process.env.CHANNEL_ID);
-  if (channel) channel.send(mensaje);
-  
-    }, 20 * 60 * 1000); // 20 minutos
   });
 
   mcBot.on('end', () => {
@@ -52,44 +32,17 @@ function createMinecraftBot() {
   });
   
   mcBot.on('playerJoined', (player) => {
-    // console.log(`${player.username} se ha unido al servidor`);
+    console.log(`${player.username} se ha unido al servidor`);
     if (player.username === 'chipinazo') {
       mcBot.chat('Creator! Welcome back genius.');
     }
-    if (player.username === '_letrasado') {
-      mcBot.chat('Welcome back Letrasado, kisses from el Copas.');
-    }
-    if (player.username === 'MrDavid99') {
-      mcBot.chat('Hey David! I also hate polish and albanians.');
-    }
-    if (player.username === 'Diakhaba') {
-      mcBot.chat('Cómo te huelen los pinrreles.');
-    }
-    if (player.username === 'marcosgo16') {
-      mcBot.chat('Ponte a trabajar bujarra!');
-    }
-    if (player.username === 'PowerXInfinito') {
-      mcBot.chat('Paño con pipí mojao.');
-    }
-
-
-
-
-  // const channel = discordClient.channels.cache.get(process.env.CHANNEL_ID);
-   //if (channel) {
-    //channel.send(`**${player.username}** se ha unido al servidor de Minecraft.`);
-   // }
     // O enviar un mensaje al chat:
     // bot.chat(`¡Bienvenido, ${player.username}!`);
   });
   
   mcBot.on('playerLeft', (player) => {
-    // console.log(`${player.username} salió del servidor`);
+    console.log(`${player.username} salió del servidor`);
     // bot.chat(`Hasta luego, ${player.username}`);
-    // const channel = discordClient.channels.cache.get(process.env.CHANNEL_ID);
-  // if (channel) {
-    // channel.send(`**${player.username}** salió del servidor de Minecraft.`);
-  // }
   });
   
 
