0f8a27f6e3dc9dcf466fed48ed8a33afd3bf8225
Javier Garcia <161944838+JavierGarciaUCLM@users.noreply.github.com>
2025-04-12 19:58:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -135,6 +135,27 @@ function createMinecraftBot() {
   });
 
   mcBot.on('chat', async (username, message) => {
+    const enterMatch = message.match(entraWorld);
+    if (enterMatch) {
+      const playerName = enterMatch[1].trim(); 
+      const channel = discordClient.channels.cache.get(process.env.CHANNEL_ID);
+      if (channel) {
+        channel.send(`${playerName} **ha entrado** de World.`);
+      }
+      return;
+    }
+
+    const leaveMatch = message.match(saleWorld);
+    if (leaveMatch) {
+      const playerName = leaveMatch[1].trim();
+      const channel = discordClient.channels.cache.get(process.env.CHANNEL_ID);
+      if (channel) {
+        channel.send(`${playerName} **ha salido** de World.`);
+      }
+      return;
+    }
+
+
   //No funciona el antispam, cómo puedo solucionarlo?
     if (isSpamming(username)) {
       mcBot.whisper(username, "You are muted for spam. Wait a minute before talking to the bot.");
@@ -238,26 +259,6 @@ function createMinecraftBot() {
         });
       return;
     }
-  
-  const enterMatch = message.match(entraWorld);
-  if (enterMatch) {
-    const playerName = enterMatch[1].trim(); 
-    const channel = discordClient.channels.cache.get(process.env.CHANNEL_ID);
-    if (channel) {
-      channel.send(`${playerName} **ha entrado** de World.`);
-    }
-    return;
-  }
-
-  const leaveMatch = message.match(saleWorld);
-  if (leaveMatch) {
-    const playerName = leaveMatch[1].trim();
-    const channel = discordClient.channels.cache.get(process.env.CHANNEL_ID);
-    if (channel) {
-      channel.send(`${playerName} **ha salido** de World.`);
-    }
-    return;
-  }
   });
 }
 
